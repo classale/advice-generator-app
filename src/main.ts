0da6be17f52b11e50e0e136414fe7a502e0e6de2
classale <99489927+classale@users.noreply.github.com>
2025-05-19 16:06:41 +0200

    feat(app): Initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+const generate = document.querySelector<HTMLButtonElement>("#generate")!;
+const adviceId = document.querySelector<HTMLHeadingElement>("#adviceId")!;
+const advice = document.querySelector<HTMLParagraphElement>("#advice")!;
+
+generate.addEventListener("click", async () => {
+  const resp = await fetch("https://api.adviceslip.com/advice");
+  const data = await resp.json();
+
+  console.log(data.slip.id)
+  adviceId.innerHTML = `ADVICE #${data.slip.id}`
+  advice.innerHTML = `"${data.slip.advice}"`
+})
